28802801bdfae469c21989bf7cf60e94ebcdc1b0
Viktor Lindblom <viktor.lindblom@gmail.com>
2020-02-13 10:06:50 +0100

    Added a simple distribution for testing and plot of mean of y

--- data_generator.py
+++ data_generator.py
@@ -1,6 +1,7 @@
 from distributions import SimpleDistribution
 import numpy as np
 
+
 def generate_data(generator, n_samples):
     data = {'z': [], 'x': [], 'h': []}
 
@@ -26,17 +27,18 @@ def trim_data(data, threshold):
     for i in range(len(trimmed_data['z'])):
         tmp_h = trimmed_data['h'][i]
         for j in range(len(tmp_h)):
-            if tmp_h[j][1] > threshold:
+            if tmp_h[j][1] >= threshold:
                 trimmed_data['h'][i] = tmp_h[0:j+1]
                 break
 
     return trimmed_data
 
-data = generate_data(SimpleDistribution(), 10)
+
+data = generate_data(SimpleDistribution(), 100)
 
 for z, x, h in zip(data['z'], data['x'], data['h']):
     print(z, x, h)
 print("-----------")
-data2 = trim_data(data, 1)
+data2 = trim_data(data, 2)
 for z, x, h in zip(data2['z'], data2['x'], data2['h']):
     print(z, x, h)
--- distributions.py
+++ distributions.py
@@ -1,7 +1,8 @@
 import numpy as np
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-
-class Distribution():
+class Distribution:
 
     def __init__(self, seed=None):
         self.random = np.random.RandomState()
@@ -20,7 +21,7 @@ class Distribution():
         return False
 
 
-class SimpleDistribution(Distribution):
+class TestDistribution(Distribution):
     n_treatments = 3
 
     def __init__(self):
@@ -55,3 +56,58 @@ class SimpleDistribution(Distribution):
             y = self.random.normal(z - 1)
         return y
 
+
+class SimpleDistribution(Distribution):
+    n_treatments = 4
+
+    def draw_z(self):
+        return self.random.randint(0, 3)
+
+    def draw_x(self, z):
+        x0 = round(min(max(self.random.normal(z/2), 0), 1))
+        x1 = self.random.randint(0, 3)
+        return [x0, x1]
+
+    def draw_a(self, h, x):
+        possible_a = range(0, 4)
+        if len(h) > 0:
+            used_a = [x[0] for x in h]
+        else:
+            used_a = []
+        draw_a = [x for x in possible_a if x not in used_a]
+        self.random.shuffle(draw_a)
+        return draw_a[0]
+
+    def draw_y(self, a, h, x, z):
+        if a == 0:
+            result = self.random.normal(0.8) - x[0]*self.random.uniform(0, 0.45) + x[1]*self.random.uniform(0, 0.45)
+        elif a == 1:
+            result = self.random.normal(0.6) - x[0] * self.random.uniform(0, 0.45) \
+                     + x[1] * self.random.uniform(0, 0.45) + z*self.random.uniform(0, 0.45)
+        elif a == 2:
+            result = self.random.normal(0.6) + z*self.random.uniform(0, 0.75)
+        elif a == 3:
+            result = self.random.normal(0.6) + x[0]*self.random.uniform(0, 1.2) \
+                     - x[1]*self.random.uniform(0, 0.5) - z*self.random.uniform(0, 0.45)
+        else:
+            return False
+        return round(min(max(result, 0), 2))
+
+
+def plot_y():
+    di = SimpleDistribution()
+    a = 3
+    freq = np.zeros((3, 2, 3))
+    for z in range(3):
+        for x0 in range(2):
+            for x1 in range(3):
+                values = []
+                for k in range(300):
+                    values.append(di.draw_y(a, None, [x0, x1], z))
+                freq[z][x0][x1] = np.mean(values)
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        ax.set_zlim((0, 2))
+        X0, X1 = np.meshgrid([0, 1], [0, 1, 2])
+        surf = ax.plot_wireframe(X0, X1, freq[z].T)
+        plt.show()
