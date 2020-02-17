cf95aabd11430e34eaa226415999deb9784f4ddd
Viktor Lindblom <viktor.lindblom@gmail.com>
2020-02-17 16:11:29 +0100

    Changed reward for stop action

--- OfflineQLearning/q_learning.py
+++ OfflineQLearning/q_learning.py
@@ -7,13 +7,17 @@ learning_rate = 0.2
 discount_factor = 0.9
 
 # Q-table indexed with x, y_0, y_1, y_2, y_3 and a
-q_table = np.zeros((n_x,) + (n_results + 1,) * n_actions + (n_actions,))
+q_table = np.zeros((n_x,) + (n_results + 1,) * n_actions + (n_actions + 1,))
 
 
 def learn(history):
     for _ in range(10000):
         state, action, reward, next_state = history[np.random.randint(0, len(history))]
-        q_table[state, action] = q_table[state, action] + learning_rate \
+
+        if action == 0:
+            q_table[state, action] = reward
+        else:
+            q_table[state, action] = q_table[state, action] + learning_rate \
                                  * (reward + discount_factor * max(q_table[next_state]) - q_table[state, action])
     print(q_table)
 
