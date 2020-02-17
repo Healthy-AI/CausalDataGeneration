18f77f2730b7a5df7ddce6f63ad6e98254d49ffb
Viktor Lindblom <viktor.lindblom@gmail.com>
2020-02-17 16:49:40 +0100

    Updated stop reward again

--- OfflineQLearning/q_learning.py
+++ OfflineQLearning/q_learning.py
@@ -14,12 +14,16 @@ def learn(history):
     for _ in range(10000):
         state, action, reward, next_state = history[np.random.randint(0, len(history))]
 
-        if action == 0:
-            q_table[state, action] = reward
-        else:
-            q_table[state, action] = q_table[state, action] + learning_rate \
+        q_table[state, action] = q_table[state, action] + learning_rate \
                                  * (reward + discount_factor * max(q_table[next_state]) - q_table[state, action])
+        stop_reward = -np.infty
+        if max(next_state[1:]) > 0:
+            stop_reward = max(next_state[1:])
+        q_table[next_state, -1] = stop_reward
+
+
     print(q_table)
+
 
 def transform_data():
     pass
