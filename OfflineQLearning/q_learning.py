5933421539ed6333f51348deffe8cd758fbf93d2
Viktor Lindblom <viktor.lindblom@gmail.com>
2020-02-18 10:32:30 +0100

    Fixed a small bug, works ok now

--- OfflineQLearning/q_learning.py
+++ OfflineQLearning/q_learning.py
@@ -26,7 +26,7 @@ def learn(history):
                         if q_table[x][y_0][y_1][y_2][y_3][-1] < 1:
                             q_table[x][y_0][y_1][y_2][y_3][-1] = -np.infty
 
-    for _ in range(10000):
+    for _ in range(100000):
         state, action, reward, next_state = history[np.random.randint(0, len(history))]
 
         q_table[to_index(state) + (action,)] = q_table[to_index(state) + (action,)] + learning_rate \
@@ -54,7 +54,7 @@ def convert_to_sars(data, n_actions):
                 a = new_action
                 r = -0.40
                 new_actions = temp_actions.copy()
-                new_actions[new_action] = 1
+                new_actions[new_action] = h[i][j][1]
                 s_prime = [patient, new_actions]
                 sars = (s, a, r, s_prime)
                 all_sars.append(sars)
@@ -73,7 +73,3 @@ def reward(history):
 data = read_json("..\DataGeneratorTest\simple_split")
 data = convert_to_sars(data, n_actions)
 q = learn(data)
-
-
-print(q[1][-1][-1][-1][-1])
-print(q[1])
