6489f57b94b72612f5eacf149217c0b4913601b2
Samuel Håkansson <samuel.hakansson@live.com>
2020-02-27 14:19:55 +0100

    think greedy works as intended

--- OfflineQLearning/greedy.py
+++ OfflineQLearning/greedy.py
@@ -7,21 +7,18 @@ class Greedy:
         self.n_x = n_x
         self.n_y = n_y
         self.n_a = n_a
-        self.probabilities = np.zeros((n_a+1, n_a+1))
+        self.probabilities = np.zeros((n_a+1, n_a+1, n_a))
 
     def find_probabilities(self, data):
         h = data['h']
 
-        treatments_effective = np.zeros((self.n_a+1, self.n_a+1))
-        treatments_total = np.zeros((self.n_a+1, self.n_a+1))
-        #treatments_effective = [0]*n_actions
-        #treatments_total = [0]*n_actions
+        treatments_effective = np.zeros((self.n_a+1, self.n_a+1, self.n_a))
+        treatments_total = np.zeros((self.n_a+1, self.n_a+1, self.n_a))
 
         for history in h:
             intervention = history.pop(-1)
             action, effectiveness = intervention
-            index = self._find_index_in_history(history)
-            print(index)
+            index = self._find_index_in_history(history, action)
             treatments_effective[index] += effectiveness
             treatments_total[index] += 1
 
@@ -30,16 +27,19 @@ class Greedy:
 
         self.probabilities = np.divide(treatments_effective, treatments_total)
 
-    def _find_index_in_history(self, history):
-        index = [-1]*(self.n_a-1)
+    def _find_index_in_history(self, history, action):
+        index = [-1]*self.n_a
         for i, h in enumerate(history):
             index[i] = h[0]
+        index[-1] = action
         return tuple(index)
+    # First index is first treatment, second index is second treatment, third index is intervention
+    # E.g. [0, -1, 1] means that we tried treatment 0 and are now intervening with treatment 1.
+
 
 n_actions = 3
 greedyAlgorithm = Greedy(1, 2, n_actions)
-data = generate_data(FredrikDistribution(), 500)
+data = generate_data(FredrikDistribution(), 1000)
 data = split_patients(data)
 greedyAlgorithm.find_probabilities(data)
-print(greedyAlgorithm.probabilities)
-
+print(greedyAlgorithm.probabilities[2, -1])
