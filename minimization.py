905a174dee44ab3534c6c8daa3c3614305dfe41b
Viktor Lindblom <viktor.lindblom@gmail.com>
2020-02-27 15:47:29 +0100

    Added a new advanced distribution

--- minimization.py
+++ minimization.py
@@ -13,9 +13,10 @@ cons = (
     {'type': 'ineq', 'fun': lambda z: z[4] - 0.05},
     {'type': 'ineq', 'fun': lambda z: z[5] - 0.05},
     {'type': 'ineq', 'fun': lambda z: z[4] - z[5]},
-    {'type': 'ineq', 'fun': lambda z: z[0] + z[1] - z[0] - z[3] - 0.05},   # E(A0) > E(A1)
-    {'type': 'ineq', 'fun': lambda z: z[0] + z[3] - z[2] - z[5] - 0.05},   # E(A1) > E(A2)
+    {'type': 'ineq', 'fun': lambda z: z[0] + z[1] - z[0] - z[3]},   # E(A0) > E(A1)
+    {'type': 'ineq', 'fun': lambda z: z[0] + z[3] - z[2] - z[5]},   # E(A1) > E(A2)
     {'type': 'ineq', 'fun': lambda z: z[0] - z[4]},
+    {'type': 'ineq', 'fun': lambda z: z[2] + z[5] - z[3]},
     {'type': 'ineq', 'fun': lambda z: z[1] + z[2] - z[4]},
     {'type': 'ineq', 'fun': lambda z: z[2] + z[3] - z[4]},
     {'type': 'ineq', 'fun': lambda z: ((z[0] + z[1])*1 + (z[2] + z[5])*2 + (z[3] + z[4])*3) - ((z[0] + z[3])*1 + (z[1] + z[2] + z[4] + z[5])*2)},   # E(A1) < E(A0) long term
