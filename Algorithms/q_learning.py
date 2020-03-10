fb25ef3c121d3984cc4e97069e87407ffd5b11d8
Viktor Lindblom <viktor.lindblom@gmail.com>
2020-03-10 11:12:22 +0100

    Added an evaluate function to compare the different algorithms

--- Algorithms/q_learning.py
+++ Algorithms/q_learning.py
@@ -1,22 +1,28 @@
 from DataGenerator.data_generator import *
 from DataGenerator.data_visualizer import *
 
+
 class QLearner:
-    def __init__(self, n_x, n_y, n_a, learning_rate=0.01, discount_factor=1):
+    def __init__(self, n_x, n_a, n_y, data, reward=-0.1, learning_rate=0.01, discount_factor=1):
         self.n_x = n_x
         self.n_y = n_y
         self.n_a = n_a
+        self.data = data
+        self.step_reward = reward
         self.learning_rate = learning_rate
         self.discount_factor = discount_factor
         self.q_table = None
 
+        self.sars_data = self.convert_to_sars()
+
     def to_index(self, state):
         return tuple(np.hstack(state))
 
-    def learn(self, history):
+    def learn(self):
         # Q-table indexed with x, y_0, y_1, y_2, y_3 and a
         self.q_table = np.zeros((2,) * self.n_x + (self.n_y + 1,) * self.n_a + (self.n_a + 1,))
         modified = self.q_table.copy()
+        history = self.sars_data
 
         # Initialize all final states with the rewards for picking that state
         for x in range(len(self.q_table)):
@@ -27,7 +33,7 @@ class QLearner:
                     self.q_table[self.to_index([x, y_t, -1])] = -np.infty
                 modified[self.to_index(([x, y_t, -1]))] = True
 
-        for k in range(200000):
+        for k in range(2000):
             state, action, reward, next_state = history[np.random.randint(0, len(history))]
 
             self.q_table[self.to_index(state) + (action,)] = self.q_table[self.to_index(state) + (action,)] \
@@ -37,7 +43,7 @@ class QLearner:
             modified[self.to_index(state) + (action,)] = True
 
         # Hack to make all unmodified cells a bad action
-        self.q_table += (modified-1) * 1000000.0
+        self.q_table += (modified-1) * self.n_a * self.n_y * 10.0
         return self.q_table
 
     def evaluate(self, subject):
@@ -53,40 +59,48 @@ class QLearner:
             y[action] = y_fac[action]
             history.append([action, y[action]])
             state = np.array([x, y])
-            action = np.argmax(self.q_table[self.to_index(state)])
+            action_candidates = np.argwhere(self.q_table[self.to_index(state)] == np.max(self.q_table[self.to_index(state)])).flatten()
+            if len(action_candidates) == 1:
+                action = action_candidates[0]
+            else:
+                # TODO implement a better method for choosing when no valid action is available!
+                # TODO choose the treatment with highest mean effect for this x?
+                print("Choosing action arbitrarily")
+                for a in action_candidates:
+                    if y[action_candidates[a]] == -1:
+                        action = action_candidates[a]
+                        break
         return history
 
+    def convert_to_sars(self):
+        x = self.data['x']
+        h = self.data['h']
+        all_sars = []
+        for i, patient in enumerate(x):
+            actions = np.array([-1] * self.n_a)
+            for treatment in h[i]:
+                action, outcome = treatment
+                actions[action] = outcome
 
-def convert_to_sars(data, n_actions):
-    x = data['x']
-    h = data['h']
-    all_sars = []
-    for i, patient in enumerate(x):
-        actions = np.array([-1] * n_actions)
-        for treatment in h[i]:
-            action, outcome = treatment
-            actions[action] = outcome
+            for j in range(len(h[i])):
+                temp_actions = actions.copy()
+                new_action = h[i][j][0]
+                temp_actions[new_action] = -1
+                s = np.array([patient, temp_actions])
+                a = new_action
+                r = self.step_reward
+                new_actions = temp_actions.copy()
+                new_actions[new_action] = h[i][j][1]
+                s_prime = np.array([patient, new_actions])
+                sars = (s, a, r, s_prime)
+                all_sars.append(sars)
+        return all_sars
 
-        for j in range(len(h[i])):
-            temp_actions = actions.copy()
-            new_action = h[i][j][0]
-            temp_actions[new_action] = -1
-            s = np.array([patient, temp_actions])
-            a = new_action
-            r = -0.1
-            new_actions = temp_actions.copy()
-            new_actions[new_action] = h[i][j][1]
-            s_prime = np.array([patient, new_actions])
-            sars = (s, a, r, s_prime)
-            all_sars.append(sars)
-    return all_sars
-
-
+'''
 n_x = 1
 n_z = 3
 n_a = 5
 n_y = 3
-ql = QLearner(n_x, n_y, n_a, learning_rate=0.01)
 for i in range(1):
     dist = DiscreteDistribution(n_z, n_x, n_a, n_y, seed=0)
     data = generate_data(dist, 15000)
@@ -95,10 +109,11 @@ for i in range(1):
     DV.print_y(data, np.array([0]))
     DV.print_y(data, np.array([1]))
     data = split_patients(data)
-    data = convert_to_sars(data, n_a)
-    q = ql.learn(data)
+    ql = QLearner(n_x, n_a, n_y, data, learning_rate=0.01)
+    q = ql.learn()
     test_data = generate_test_data(dist, 100)
     for j in range(100):
         print(test_data[j][0], test_data[j][1], ql.evaluate(test_data[j]))
     print(q[0, -1, -1, -1, -1, -1])
     print(q[1, -1, -1, -1, -1, -1])
+'''
--- Main/Evaluate.py
+++ Main/Evaluate.py
@@ -1,18 +1,110 @@
-import Algorithms.constrained_q_learning as cq_learning
-import Algorithms.q_learning as q_learning
 import Algorithms.greedyShuffledHistory as greedy
-from DataGenerator.distributions import *
+import matplotlib.pyplot as plt
 from DataGenerator.data_generator import *
+import Algorithms.q_learning as ql
+import time
 
-n_actions = 3
-n_outcomes = 3
-max_possible_outcome = 2
-stop_action = 3
-greedyAlgorithm = greedy.GreedyShuffled(1, 3, n_actions)
-data = generate_data(NewDistribution(), 3000)
+seed = 0
+n_z = 3
+n_x = 1
+n_a = 5
+n_y = 3
+n_algorithms = 1
+n_training_samples = 1000
+n_test_samples = 100
+main_start = time.time()
+plot_colors = ['k', 'r--', 'b:', 'g-.']
 
-data_dict = split_patients(data)
-greedyAlgorithm.find_probabilities(data)
+# Generate the data
+dist = DiscreteDistribution(n_z, n_x, n_a, n_y, seed=seed)
+start = time.time()
+print("Generating {} training samples...".format(n_training_samples))
+training_data = generate_data(dist, n_training_samples)
+split_training_data = split_patients(training_data)
+print("Generating training samples took {:.3f} seconds".format(time.time()-start))
+start = time.time()
+print("Generating {} test samples...".format(n_test_samples))
+test_data = generate_test_data(dist, n_test_samples)
+print("Generating test samples took {:.3f} seconds".format(time.time()-start))
 
-r = greedyAlgorithm.probabilities[-1, -1, -1]
-print(r)
+
+# Initialize and train the algorithms
+# TODO Init Greedy
+# start = time.time()
+# print("Initializing Greedy...")
+# G = greedy.GreedyShuffled(n_x, n_y, n_a)
+# print("\tTraining Greedy...")
+# G.find_probabilities(training_data)
+# print("\tTraining the Greedy algorithm took {:.3f} seconds".format(time.time()-start))
+# TODO Init Constrained Q Learning
+# start = time.time()
+# print("Initializing Constrained Q-learning...")
+# CQL = cql.ConstrainedQlearner(n_x, n_y, n_a, split_training_data, learning_rate=0.01, discount_factor=1)
+# print("\tTraining Constrained Q-learning...")
+# CQL.learn()
+# print("\tTraining the Constrained Q-learning algorithm took {:.3f} seconds".format(time.time()-start))
+start = time.time()
+print("Initializing Q-learning...")
+QL = ql.QLearner(n_x, n_a, n_y, split_training_data, reward=-0.1, learning_rate=0.01, discount_factor=1)
+print("\tTraining Q-learning...")
+QL.learn()
+print("\tTraining the Q-learning algorithm took {:.3f} seconds".format(time.time()-start))
+
+# Evaluate the algorithms
+evaluations = [[]]
+for i in range(n_test_samples):
+    # evaluations[0].append(
+    # evaluations[1].append(CQL.evaluate(test_data[i]))
+    e = QL.evaluate(test_data[i])
+    print(e)
+    evaluations[0].append(QL.evaluate(test_data[i]))
+
+# Calculate mean treatment effect over population
+mean_treatment_effects = np.zeros((n_algorithms, n_a + 1))      # Overshoot by 1 to get all max values at last step
+for i_alg in range(n_algorithms):
+    for i_sample in range(n_test_samples):
+        treatments = evaluations[i_alg][i_sample]
+        best_found = 0
+        for i_treatment in range(len(mean_treatment_effects[i_alg])):
+            if i_treatment >= len(treatments):
+                effect = best_found
+            else:
+                effect = treatments[i_treatment][1]
+            if effect < best_found:
+                print(i_treatment)
+            if effect > best_found:
+                best_found = effect
+            mean_treatment_effects[i_alg][i_treatment] += effect
+mean_treatment_effects /= n_test_samples
+# Plot mean treatment effect over population
+x = np.arange(0, n_a+1)
+for i_plot in range(n_algorithms):
+    plt.plot(x, mean_treatment_effects[0], plot_colors[i_plot])
+plt.xlabel('Treatment index')
+plt.ylabel('Mean treatment effect over population')
+plt.show()
+
+# Calculate max mean treatment effect over population
+max_mean_treatment_effects = np.zeros((n_algorithms, n_a))
+for i_alg in range(n_algorithms):
+    for i_sample in range(n_test_samples):
+        treatments = evaluations[i_alg][i_sample]
+        best_found = 0
+        for i_treatment in range(len(max_mean_treatment_effects[i_alg])):
+            if i_treatment < len(treatments):
+                effect = treatments[i_treatment][1]
+                if effect > best_found:
+                    best_found = effect
+            max_mean_treatment_effects[i_alg][i_treatment] += best_found
+max_mean_treatment_effects /= n_test_samples
+# Plot mean treatment effect over population
+x = np.arange(0, n_a)
+for i_plot in range(n_algorithms):
+    plt.plot(x, max_mean_treatment_effects[0], plot_colors[i_plot])
+plt.xlabel('Treatment index')
+plt.ylabel('Max mean treatment effect over population')
+plt.show()
+
+
+
+print("Running Evaluate took {:.3f} seconds".format(time.time()-main_start))
