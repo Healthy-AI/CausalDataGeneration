6097e969ac39896191c3a30f7e2569a89997cc7c
SamuelHakansson <samuel.hakansson@live.com>
2020-03-11 15:53:58 +0100

    updated testing

--- Main/Test.py
+++ Main/Test.py
@@ -1,4 +1,5 @@
 from Algorithms.constrained_q_learning import ConstrainedQlearner
+from Algorithms.greedyShuffledHistory import GreedyShuffled
 from DataGenerator.data_generator import *
 from Algorithms.q_learning import QLearner
 
@@ -7,27 +8,27 @@ n_actions = 3
 n_outcomes = 3
 max_possible_outcome = 2
 stop_action = 3
-data = generate_data(NewDistribution(), 500)
+data = generate_data(NewDistribution(), 1000)
 
 data = split_patients(data)
-cql = ConstrainedQlearner(1, n_actions, n_outcomes, data, learning_rate=0.01)
-cql.fill_table()
-cq = cql.q_table
 
+cql = ConstrainedQlearner(1, n_actions, n_outcomes, data, learning_rate=0.01)
+cql.learn()
+cq = cql.q_table
+print('''print(cq[0, -1, -1, -1])
+print(cq[0, 0, -1, -1]),
+print(cq[0, -1, -1, 2]),
+print(cq[0, -1, -1, 0])''')
 print(cq[0, -1, -1, -1])
-print(cq[0, 1, -1, -1])
-print(cq[0, 1, 2, -1])
-print(cq[0, 2, -1, -1])
-print(cq[0, 1, 1, -1])
-print(cq[0, -1, 1, 1])
+print(cq[0, 0, -1, -1])
+print(cq[0, -1, -1, 2])
+print(cq[0, -1, -1, 0])
 
 print('---------')
 qlearner = QLearner(1, n_actions, n_outcomes, data, learning_rate=0.01)
 q = qlearner.learn()
 
 print(q[0, -1, -1, -1])
-print(q[0, 1, -1, -1])
-print(q[0, 1, 2, -1])
-print(q[0, 2, -1, -1])
-print(q[0, 1, 1, -1])
-print(q[0, -1, 1, 1])
+print(q[0, 0, -1, -1])
+print(q[0, -1, -1, 2])
+print(q[0, -1, -1, 0])
