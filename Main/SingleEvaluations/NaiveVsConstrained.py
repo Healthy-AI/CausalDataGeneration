15c9194bf33164b4f709a06a821b923bd423a459
Viktor Lindblom <viktor.lindblom@gmail.com>
2020-04-27 14:37:28 +0200

    Made a simple delta-sweep file

--- Main/SingleEvaluations/NaiveVsConstrained.py
+++ Main/SingleEvaluations/NaiveVsConstrained.py
@@ -1,21 +1,16 @@
-from Algorithms.function_approximation import FunctionApproximation
 from Algorithms.naive_dynamic_programming import NaiveDynamicProgramming
 from Algorithms.constrained_dynamic_programming import ConstrainedDynamicProgramming
 from Algorithms.naive_greedy import NaiveGreedy
-from Algorithms.deep_q_learning import DeepQLearning
 from Algorithms.constrained_greedy import ConstrainedGreedy
-from Algorithms.true_approximator import TrueApproximator
 from DataGenerator.data_generator import *
 import time
-from pathlib import Path
 from Algorithms.better_treatment_constraint import Constraint
 from Algorithms.statistical_approximator import StatisticalApproximator
-from Database.antibioticsdatabase import AntibioticsDatabase
 
 if __name__ == '__main__':
     # Training values
     #seed = 90821  # Used for both synthetic and real data
-    n_tests = 10
+    n_tests = 100
     seeds = [np.random.randint(0, 10000) for i in range(n_tests)]
     print(seeds)
     n_z = 3
@@ -23,14 +18,12 @@ if __name__ == '__main__':
     n_a = 5
     n_y = 3
     training_episodes = 750000
-    n_training_samples = 30000
-    n_test_samples = 3000
-    delta = 0.0
+    n_training_samples = 5000
+    n_test_samples = 1500
+    delta = 0.3
     epsilon = 0
     reward = -0.35
     # for grid search
-    nr_deltas = 40
-    delta_limit = 1
 
     # Plot values
     treatment_slack = 0  # Eg, how close to max must we be to be considered "good enough"
@@ -52,97 +45,39 @@ if __name__ == '__main__':
     main_start = time.time()
 
     n_algorithms = 4
-    mean_n_tests_results = np.zeros((n_tests, n_algorithms))
-    efficiency_results = np.zeros((n_tests, n_algorithms))
+    mean_n_tests_results = np.zeros((n_algorithms, n_tests))
+    efficiency_results = np.zeros((n_algorithms, n_tests))
     for i, seed in enumerate(seeds):
         # Generate the data
-        # dist = DiscreteDistribution(n_z, n_x, n_a, n_y, seed=seed, outcome_sensitivity_x_z=1)
         dist = DiscreteDistributionWithSmoothOutcomes(n_z, n_x, n_a, n_y, seed=seed, outcome_sensitivity_x_z=1)
-        # dist = DiscreteDistributionWithInformation(n_z, n_x, n_a, n_y, seed=seed)
-        #'''
         dist.print_moderator_statistics()
         dist.print_covariate_statistics()
         dist.print_treatment_statistics()
         dist.print_detailed_treatment_statistics()
-        #'''
-        #dist = AntibioticsDatabase(n_x=2, antibiotic_limit=6, seed=seed)
 
-        if type(dist) != AntibioticsDatabase:
-            training = {'name': 'training', 'samples': n_training_samples, 'func': generate_data, 'split': True,
-                        'database': False}
-            test = {'name': 'test', 'samples': n_test_samples, 'func': generate_test_data, 'split': False,
-                    'database': False}
-            datasets = {'training': training, 'test': test}
+        print("Generating data")
+        training_data = generate_data(dist, n_training_samples)
+        test_data = generate_test_data(dist, n_test_samples)
 
-            for key, dataset in datasets.items():
-                filename = '{}{}{}{}vars{}{}{}{}.h5'.format(
-                    dist.name, str(dataset['samples']), dataset['name'], seed,
-                    n_z, n_x, n_a, n_y)
-                filepath = Path('Data', filename)
-                '''
-                try:
-                    data = dd.io.load(filepath)
-                    dataset['data'] = data
-                    print('Found %s data on file' % dataset['name'])
-                except IOError:
-                '''
-                if True:
-                    start = time.time()
-                    n_samples = dataset['samples']
-                    print("Generating {} {} samples...".format(n_samples, dataset['name']))
-                    generate_data_func = dataset['func']
-                    if dataset['database']:
-                        data = generate_data_func(n_samples)
-                    else:
-                        data = generate_data_func(dist, n_samples)
-                    if dataset['split']:
-                        data = split_patients(data)
-                    print("Generating samples took {:.3f} seconds".format(time.time() - start))
-                    dataset['data'] = data
-                    # if seed is not None:
-                    #    dd.io.save(filepath, data)
-        else:
-            training_data, test_data = dist.get_data()
-            datasets = {'training': {'data': split_patients(training_data)}, 'test': {'data': test_data}}
-            n_x = dist.n_x
-            n_a = dist.n_a
-            n_y = dist.n_y
-            n_test_samples = len(test_data)
+        split_training_data = split_patients(training_data)
 
-        split_training_data = datasets['training']['data']
-        test_data = datasets['test']['data']
-        # print("Initializing function approximator")
-        # start = time.time()
-        function_approximation = FunctionApproximation(n_x, n_a, n_y, split_training_data)
-        # print("Initializing {} took {:.3f} seconds".format(function_approximation.name, time.time()-start))
         print("Initializing statistical approximator")
-        start = time.time()
-        statistical_approximation = StatisticalApproximator(n_x, n_a, n_y, split_training_data, prior_mode='none')
+        statistical_approximation = StatisticalApproximator(n_x, n_a, n_y, split_training_data, prior_mode='gaussian')
         # print("Initializing {} took {:.3f} seconds".format(statistical_approximation.name, time.time() - start))
-
-        true_approximation = TrueApproximator(dist)
 
         print("Initializing Constraint")
         start = time.time()
 
         constraintStatUpper = Constraint(split_training_data, n_a, n_y, approximator=statistical_approximation, delta=delta,
                                          epsilon=epsilon, bound='upper')
-       # constraintStatLower = Constraint(split_training_data, n_a, n_y, approximator=statistical_approximation, delta=delta,
-       #                                  epsilon=epsilon, bound='lower')
-        # constraintTrue = Constraint(split_training_data, n_a, n_y, approximator=true_approximation, delta=delta, epsilon=epsilon)
-        constraintFuncApprox = Constraint(split_training_data, n_a, n_y, approximator=function_approximation, delta=delta,
-                                          epsilon=epsilon)
 
         print("Initializing the constraint took {:.3f} seconds".format(time.time() - start))
         print("Initializing algorithms")
         algorithms = [
-            ConstrainedGreedy(n_x, n_a, n_y, split_training_data, constraintStatUpper, statistical_approximation,
-                               name='Constrained Greedy', label='CG'),
-            ConstrainedDynamicProgramming(n_x, n_a, n_y, split_training_data, constraintStatUpper, statistical_approximation,
-                                          name='Constrained Dynamic Programming Upper', label='CDPU'),
+            ConstrainedGreedy(n_x, n_a, n_y, split_training_data, constraintStatUpper, statistical_approximation, name='Constrained Greedy', label='CG'),
+            ConstrainedDynamicProgramming(n_x, n_a, n_y, split_training_data, constraintStatUpper, statistical_approximation, name='Constrained Dynamic Programming Upper', label='CDPU'),
             NaiveGreedy(n_x, n_a, n_y, split_training_data),
             NaiveDynamicProgramming(n_x, n_a, n_y, split_training_data, statistical_approximation, reward=reward)
-
         ]
 
         n_algorithms = len(algorithms)
@@ -179,12 +114,32 @@ if __name__ == '__main__':
                             best_found = effect
                     if best_found == best_possible:
                         best_founds_efficiency[i_alg][i_treatment] += 1
-        best_founds_efficiency /= n_test_samples
-        mean_num_tests /= n_test_samples
-        efficiency_results[i] = best_founds_efficiency
-        mean_n_tests_results[i] = mean_num_tests
-    print([alg.name for alg in algorithms])
-    print(mean_n_tests_results)
-    print(efficiency_results)
+            best_founds_efficiency /= n_test_samples
+            mean_num_tests /= n_test_samples
+            efficiency_results[i_alg][i] = best_founds_efficiency[i_alg][n_a]
+            mean_n_tests_results[i_alg][i] = mean_num_tests[i_alg]
 
+    mean_effects = np.zeros(n_algorithms)
+    mean_times = np.zeros(n_algorithms)
+    var_effects = np.zeros(n_algorithms)
+    var_times = np.zeros(n_algorithms)
+    for i in range(n_algorithms):
+        for j in range(n_tests):
+            mean_effects[i] += efficiency_results[i][j]
+            mean_times[i] += mean_n_tests_results[i][j]
+    mean_effects /= n_tests
+    mean_times /= n_tests
+    for i in range(n_algorithms):
+        for j in range(n_tests):
+            var_effects[i] += (efficiency_results[i][j] - mean_effects[i])**2
+            var_times[i] += (mean_n_tests_results[i][j] - mean_times[i])**2
+    var_effects /= n_tests - 1
+    var_times /= n_tests - 1
+
+    for i in range(n_algorithms):
+        print(algorithms[i].name)
+        print("Mean effect: {}".format(mean_effects[i]))
+        print("Mean times: {}".format(mean_times[i]))
+        print("Var effect: {}".format(var_effects[i]))
+        print("Var times: {}".format(var_times[i]))
 print("Done!")
