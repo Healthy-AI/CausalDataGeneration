79f9610a535c3c1649887443c4a22f237ae40bd0
Viktor Lindblom <viktor.lindblom@gmail.com>
2020-05-18 16:08:42 +0200

    Fix

--- Main/SingleEvaluations/SweepDelta.py
+++ Main/SingleEvaluations/SweepDelta.py
@@ -6,12 +6,13 @@ import time
 from Algorithms.constrained_dynamic_programming import generate_data, split_patients
 from DataGenerator.distributions import DiscreteDistributionWithSmoothOutcomes
 from Main.SingleEvaluations import DeltaSweepSettings, GApproximatorsSettings, CDPApproximatorsSettings, \
-    TrueApproxSettings, DeltaSweepSettings_small, NaiveVsConstrainedSettings, CDPBoundsSettings, GBoundsSettings
+    TrueApproxSettings, DeltaSweepSettings_small, NaiveVsConstrainedSettings, CDPBoundsSettings, GBoundsSettings, \
+    GeneralDeltaSweepSettings
 from Main.SingleEvaluations.PlotSweepDelta import plot_sweep_delta
 
 
 def get_settings():
-    return GBoundsSettings
+    return GeneralDeltaSweepSettings
 
 def do_work(i_data_set, n_algorithms):
     settings = get_settings()
