0e17cebcb5503c86a67d5fa31ce55a6e5aea7fe2
Viktor Lindblom <viktor.lindblom@gmail.com>
2020-06-02 11:32:49 +0200

    Added two files to plot the time and efficacy graphs separately

--- Main/SingleEvaluations/DataAmountSettings3.py
+++ Main/SingleEvaluations/DataAmountSettings3.py
@@ -4,7 +4,6 @@ from Algorithms.better_treatment_constraint import Constraint
 from Algorithms.constrained_dynamic_programming import ConstrainedDynamicProgramming
 from Algorithms.constrained_greedy import ConstrainedGreedy
 from Algorithms.exact_approximator import ExactApproximator
-from Algorithms.function_approximation import FunctionApproximation
 from Algorithms.naive_dynamic_programming import NaiveDynamicProgramming
 from Algorithms.naive_greedy import NaiveGreedy
 from Algorithms.statistical_approximator import StatisticalApproximator
@@ -27,37 +26,36 @@ def setup_algorithms(training_data, dist, delta):
     n_x = dist.n_x
     n_a = dist.n_a
     n_y = dist.n_y
-    statistical_approximation_none = StatisticalApproximator(n_x, n_a, n_y, training_data, prior_mode='none')
     statistical_approximation_prior = StatisticalApproximator(n_x, n_a, n_y, training_data, prior_mode='gaussian')
-    function_approximation = FunctionApproximation(n_x, n_a, n_y, training_data)
+    statistical_approximation_none = StatisticalApproximator(n_x, n_a, n_y, training_data, prior_mode='none')
     true_approximation = ExactApproximator(dist)
-
-    constraint_none = Constraint(training_data, n_a, n_y, approximator=statistical_approximation_none, delta=delta)
     constraint_prior = Constraint(training_data, n_a, n_y, approximator=statistical_approximation_prior, delta=delta)
-    constraint_func = Constraint(training_data, n_a, n_y, approximator=function_approximation, delta=delta)
+    constraint_none = Constraint(training_data, n_a, n_y, approximator=statistical_approximation_none, delta=delta)
     constraint_true = Constraint(training_data, n_a, n_y, approximator=true_approximation, delta=delta)
-
     algorithms = [
-        ConstrainedDynamicProgramming(n_x, n_a, n_y, training_data, constraint_prior, statistical_approximation_prior, name="Dynamic Programming Historical Prior", label="CDP_H"),
-        ConstrainedGreedy(n_x, n_a, n_y, training_data, constraint_func, function_approximation, name="Constrained Greedy Historical Prior", label="CG_H"),
-        NaiveDynamicProgramming(n_x, n_a, n_y, training_data, statistical_approximation_prior, reward=-(delta+0.0001), name='Naive Dynamic Programming Historical Prior', label='NDP_H'),
-        NaiveGreedy(n_x, n_a, n_y, approximator=statistical_approximation_prior, max_steps=4),
+        ConstrainedDynamicProgramming(n_x, n_a, n_y, training_data, constraint_true, true_approximation, name="Dynamic Programming True", label="CDP_T"),
+        ConstrainedDynamicProgramming(n_x, n_a, n_y, training_data, constraint_prior, statistical_approximation_prior, name="Constrained Dynamic Programming", label="CDP_H"),
+        ConstrainedDynamicProgramming(n_x, n_a, n_y, training_data, constraint_none, statistical_approximation_none, name="Constrained Dynamic Programming Uninformed", label="CDP_U"),
+        ConstrainedGreedy(n_x, n_a, n_y, training_data, constraint_prior, statistical_approximation_prior, name="Constrained Greedy", label="CG"),
+        #NaiveGreedy(n_x, n_a, n_y, statistical_approximation_prior, round(delta * (n_a-1))+1, name='Naive Greedy', label='NG'),
+        #NaiveDynamicProgramming(n_x, n_a, n_y, training_data, statistical_approximation_prior, reward=-(delta+0.0001), name='Naive Dynamic Programming', label='NDP'),
     ]
 
-    print("Setting up algorithms took {:.3f} seconds".format(time.time() - start))
+    print("Setting up algosrithms took {:.3f} seconds".format(time.time() - start))
     return algorithms
 
 
 def load_settings():
-    starting_seed = 80941
+    starting_seed = 476891
     n_data_sets = 10
-    n_deltas = 40
+    n_data_set_sizes = 30
+    delta = 0.3
     n_z = 3
     n_x = 1
     n_a = 5
     n_y = 3
-    n_training_samples = 7000
-    n_test_samples = 2000
-    file_name_prefix = "Naive_Vs_Constrained_"
+    n_training_samples_max = 100000
+    n_test_samples = 3000
+    file_name_prefix = "DataSweep_50k2_"
 
-    return starting_seed, n_data_sets, n_deltas, n_z, n_x, n_a, n_y, n_training_samples, n_test_samples, file_name_prefix
+    return starting_seed, n_data_sets, delta, n_data_set_sizes, n_z, n_x, n_a, n_y, n_training_samples_max, n_test_samples, file_name_prefix
